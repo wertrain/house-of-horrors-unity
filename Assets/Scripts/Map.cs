6f21bfe1c2ac16f8af402b3eb2a90b16f3a09390
wertrain <wertrain@gmail.com>
2020-07-05 18:49:00 +0900

    スクリプトからのマップ生成テスト

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = (GameObject)Resources.Load("Prefabs/Room/1Door Room");
+
+        // プレハブを元にオブジェクトを生成する
+        GameObject instance = Instantiate(obj,
+                                    new Vector3(5.0f, 0.0f, 0.0f),
+                                    Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
